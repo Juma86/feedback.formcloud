185d0a558f7e40a30e2315be0eefc3ea295f529a
Connor R Beeney <C.Beeney@outlook.com>
2025-12-02 01:27:17 +0000

    Uses hypercorn

--- uk/formcloud/feedback/api.py
+++ uk/formcloud/feedback/api.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from dataclasses import dataclass
+from dotenv import dotenv_values
+from httpx import get
+from fastapi.responses import HTMLResponse
+
 app = FastAPI()
+
+environment : dict[str, str | None] = dotenv_values('api.env')
 
 @dataclass
 class Key(BaseModel):
@@ -22,3 +28,19 @@ async def post_store_key(key: Key) -> None:
 @app.get('/retrieve-key/{keyname}')
 async def get_retrieve_key(keyname: str) -> list[str]:
     return [ item.value for item in keys if item.key == keyname ]
+
+@app.get('/google')
+async def get_google() -> HTMLResponse:
+    google_url : str | None = environment.get('GOOGLE_URL')
+
+    if google_url is None:
+        raise HTTPException(status_code=500, detail='Unable to load URL of google.')
+
+    assert google_url is not None, "No HTTPException was raised where google_url is none"
+
+    response = get(google_url)
+
+    if (status := response.status_code) != 200:
+        raise HTTPException(status_code=502, detail='Failed to read HTML from google_url, client returned code {}'.format(status))
+    
+    return HTMLResponse(response.text)
